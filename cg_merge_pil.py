36c61762750ba75502e53024ff67f351d088c27b
GuiltyInnocence <43642783+GuiltyInnocence@users.noreply.github.com>
2020-08-14 22:37:07 +0800

    Add files via upload

---
+++ cg_merge_pil.py
@@ -0,0 +1,29 @@
+﻿import os
+from PIL import Image, ImageDraw, ImageFont, ImageFilter , ImageGrab 
+from sys import argv
+
+for i in os.listdir(os.path.abspath(".")) :
+	if (i[i.rfind(".")+1:] == "pimg") :
+		os.system("expimg.exe "+i)
+		for j in os.listdir(os.path.abspath(".")) :
+			if (j[j.rfind(".")+1:] == "tlg") :
+				os.system("tlg2png.exe "+j+" "+j[:j.rfind(".")] + ".png")
+				os.remove(j)
+		nm = i[:i.rfind(".")] + "+pimg+"
+		rulen = nm + "layers.txt"
+		with open(os.path.join(rulen)) as ru:
+			rule = ru.read()
+		p1=rule.find("width:        "+rule[14:18])-5
+		base=rule[p1:p1+4].lstrip().rstrip("\n").rstrip("\r")
+		b0=Image.open(nm+base+".png")
+		ind=rule.find("layer_id:")
+		while (ind>0) :
+			cnm=rule[ind+14:rule.find("\n")].rstrip("\n").rstrip("\r")
+			b2=Image.open(nm+cnm+".png")
+			left=rule.find("left:",ind)+14
+			top=rule.find("top:",ind)+14
+			pos=(int(rule[left:rule.find("\n",left)]),int(rule[top:rule.find("\n",top)]))
+			b1=Image.open(nm+base+".png")
+			b1.paste(b2,pos,b2)
+			b1.save(cnm+"done.png","png")
+			ind=rule.find("layer_id:",ind+1)
---
+++ ping_2_xp3rule.py
@@ -0,0 +1,32 @@
+﻿import os
+
+for i in os.listdir(os.path.abspath(".")) :
+	if (i[i.rfind(".")+1:] == "txt") :
+		with open(os.path.join(i)) as ru:
+			rule = ru.read()
+		out = open(i[:i.rfind("+")]+".txt","w")
+		out.write("#layer_type	name	left	top	width	height	type	opacity	visible	layer_id	group_layer_id			\n\n")
+		pos=rule.find("name:")
+		while(pos>0):
+			name=rule[pos+12:rule.find("\n",pos+1)].lstrip()
+			pos=rule.find("layer_id:",pos)
+			layerid=rule[pos+12:rule.find("\n",pos+1)].lstrip()
+			pos=rule.find("width:",pos)
+			width=rule[pos+12:rule.find("\n",pos+1)].lstrip()
+			pos=rule.find("height:",pos)
+			height=rule[pos+12:rule.find("\n",pos+1)].lstrip()
+			pos=rule.find("left:",pos)
+			left=rule[pos+12:rule.find("\n",pos+1)].lstrip()
+			pos=rule.find("top:",pos)
+			top=rule[pos+12:rule.find("\n",pos+1)].lstrip()
+			pos=rule.find("opacity:",pos)
+			opacity=rule[pos+12:rule.find("\n",pos+1)].lstrip()
+			pos=rule.find("layer_type:",pos)
+			layertype=rule[pos+12:rule.find("\n",pos+1)].lstrip()
+			pos=rule.find("type:",pos+12)
+			type=rule[pos+12:rule.find("\n",pos+1)].lstrip()
+			pos=rule.find("visible:",pos)
+			visible=rule[pos+12:rule.find("\n",pos+1)].lstrip()
+			out.write(layertype+"	"+name+"	"+left+"	"+top+"	"+width+"	"+height+"	"+type+"	"+opacity+"	"+visible+"	"+layerid+"	0			\n")
+			pos=rule.find("name:",pos)
+		out.close()
